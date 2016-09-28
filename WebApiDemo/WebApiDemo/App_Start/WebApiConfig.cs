60a724873607e55ef473d8656bffa0119c7ae099
necipbaser - Necip Başer <necipbaser71@gmail.com>
2016-09-28 20:52:20 +0300

    Added WebApiConfig Nweton Json Formatting and CamelCaseProperty

--- WebApiDemo/WebApiDemo/App_Start/WebApiConfig.cs
+++ WebApiDemo/WebApiDemo/App_Start/WebApiConfig.cs
@@ -25,6 +25,9 @@ namespace WebApiDemo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
