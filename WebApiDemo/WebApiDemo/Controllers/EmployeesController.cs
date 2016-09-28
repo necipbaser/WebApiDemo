7e2b143391e7a157eb9b197707163effa8ede635
necipbaser - Necip Başer <necipbaser71@gmail.com>
2016-09-28 20:32:59 +0300

    Added EmployeeDataAccess Project and EmployeeDataModel. Added EmployeeController(ApiController) and get method

---
+++ WebApiDemo/WebApiDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        
+        public IEnumerable<Employees> Get()
+        {
+            using (EmployeeDBEntities empEntities = new EmployeeDBEntities())
+            {                
+                return empEntities.Employees.ToList();
+            }
+        }
+
+        public Employees Get(int id)
+        {
+            using (EmployeeDBEntities empEntities = new EmployeeDBEntities())
+            {
+                return empEntities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
